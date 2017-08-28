6dd2a523eafd0ac333db2e5d7909f1d334b9ad80
Firefly4266 <gaines.johnp@gmail.com>
2017-08-28 14:09:39 -0700

    Add project files.

---
+++ AcmeTest/CustomerFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AcmeTest
+{
+    [TestFixture]
+    public class CustomerFixture
+    {
+        private void ShowCustomer(Customer cust)
+        {
+            Console.WriteLine("id = {0}", cust.CustomerId);
+            Console.WriteLine("first = {0}", cust.FirstName);
+            Console.WriteLine("last = {0}", cust.LastName);
+            Console.WriteLine("email = {0}", cust.EmailAddress);
+        }
+        [Test]
+        public Customer(string first, string last, string email)
+        {
+            CustomerId = nextCustId++;
+            FirstName = first;
+            LastName = last;
+            EmailAddress = email;
+        }
+    }
+}
